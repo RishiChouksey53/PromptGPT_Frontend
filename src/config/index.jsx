bfeba8b841bedb4bf978904b7b3cbddf8fcebc40
Rishi <choukseyrishi89@gmail.com>
2025-07-31 06:42:20 +0530

    base_url

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8080";
-// export const BASE_URL = "https://promptgpt-backend.onrender.com";
+// export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://promptgpt-backend.onrender.com";
 
 export const clientServer = axios.create({
   baseURL: BASE_URL,
